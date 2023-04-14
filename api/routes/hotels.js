c92b93aa28bb36f4f6d5e50d3adb6865a1addad8
Eunan Diamond <eunan.diamond@oracle.com>
2023-04-14 22:25:21 +0100

    changed made to make do backend tasks on front end

--- api/routes/hotels.js
+++ api/routes/hotels.js
@@ -1,7 +1,7 @@
 import express from "express";
 import Hotel from "../models/Hotel.js";
 import { createError } from "../utils/error.js";
-import { createHotel, deleteHotel, getHotel, getHotels, updateHotel } from "../controllers/hotel.js";
+import { countByCity, countByType, createHotel, deleteHotel, getHotel, getHotels, updateHotel } from "../controllers/hotel.js";
 import { verifyAdmin } from "../utils/verifyToken.js";
 
 
@@ -15,7 +15,10 @@ router.put("/:id",  verifyAdmin,updateHotel)
 
 router.delete("/:id",  verifyAdmin ,deleteHotel)
 
-router.get("/:id",getHotel)
+router.get("/find/:id",getHotel)
 
 router.get("/",  getHotels)
+
+router.get("/countByCity",  countByCity)
+router.get("/countByType",  countByType)
 export default router
