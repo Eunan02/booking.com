ee20b225fd8830d3d185d6524bc3751c685f1ae7
Eunan Diamond <eunan.diamond@oracle.com>
2023-04-22 22:04:00 +0100

    reserveing rooms added

--- api/routes/rooms.js
+++ api/routes/rooms.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { createRoom, deleteRoom, getRoom, getRooms, updateRoom } from "../controllers/room.js";
+import { createRoom, deleteRoom, getRoom, getRooms, updateRoom, updateRoomAvailability } from "../controllers/room.js";
 import { verifyAdmin } from "../utils/verifyToken.js";
 
 
@@ -10,6 +10,7 @@ router.post("/:hotelid", verifyAdmin, createRoom)
 
 router.put("/:id",  verifyAdmin, updateRoom)
 
+router.put("/availability/:id", updateRoomAvailability)
     
   
 
